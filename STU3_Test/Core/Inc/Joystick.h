ebf563bceb7803bebb26e9ec07633cee11250673
ibepreparing <156896874+ibepreparing@users.noreply.github.com>
2025-05-08 22:53:24 +0700

    Update

--- STU3_Test/Core/Inc/Joystick.h
+++ STU3_Test/Core/Inc/Joystick.h
@@ -10,4 +10,13 @@ extern uint8_t SPIRx[PS2_DATA_LENGTH];
 
 void PS2_ReadData();
 
+uint8_t PS2_ButtonCircle();
+uint8_t PS2_ButtonSquare();
+uint8_t PS2_ButtonTriangle();
+uint8_t PS2_ButtonCross();
+uint8_t PS2_ButtonR1();
+uint8_t PS2_ButtonR2();
+uint8_t PS2_ButtonSelect();
+uint8_t PS2_ButtonStart();
+
 #endif /* INC_JOYSTICK_H_ */
--- STU3_Test/Core/Src/Joystick.c
+++ STU3_Test/Core/Src/Joystick.c
@@ -7,10 +7,46 @@ uint8_t SPIRx[PS2_DATA_LENGTH];
 #define PS2_CS_PIN  GPIO_PIN_10
 
 void PS2_ReadData() {
+	HAL_Delay(1);
 	HAL_GPIO_WritePin(PS2_CS_PORT, PS2_CS_PIN, GPIO_PIN_RESET);
+
 	for (int i = 0; i < PS2_DATA_LENGTH; i++) {
 		HAL_SPI_TransmitReceive(&hspi1, &SPITx[i], &SPIRx[i], 1, HAL_MAX_DELAY);
-		HAL_Delay(1);  // delay เล็กๆ เพื่อให้จอยตอบกลับได้ทัน
+		HAL_Delay(1);
 	}
+
 	HAL_GPIO_WritePin(PS2_CS_PORT, PS2_CS_PIN, GPIO_PIN_SET);
+	HAL_Delay(1);
+}
+
+uint8_t PS2_ButtonCircle() {
+	return !(SPIRx[4] & 0x20);
+}
+
+uint8_t PS2_ButtonSquare() {
+	return !(SPIRx[4] & 0x80);
+}
+
+uint8_t PS2_ButtonTriangle() {
+	return !(SPIRx[4] & 0x10);
+}
+
+uint8_t PS2_ButtonCross() {
+	return !(SPIRx[4] & 0x40);
+}
+
+uint8_t PS2_ButtonR1() {
+	return !(SPIRx[4] & 0x08);
+}
+
+uint8_t PS2_ButtonR2() {
+	return !(SPIRx[4] & 0x02);
+}
+
+uint8_t PS2_ButtonSelect() {
+	return !(SPIRx[3] & 0x01);
+}
+
+uint8_t PS2_ButtonStart() {
+	return !(SPIRx[3] & 0x08);
 }
