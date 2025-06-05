984d3078e6d6f54f4d61ba187940aa636912e52c
DEFFERENT <Bhumipatngam@gmail.com>
2025-06-05 21:41:02 +0700

    Update2

--- STU3_Programming-Prime/STU3_Test/Core/Src/tim.c
+++ STU3_Programming-Prime/STU3_Test/Core/Src/tim.c
@@ -296,7 +296,7 @@ void MX_TIM16_Init(void)
   htim16.Instance = TIM16;
   htim16.Init.Prescaler = 169;
   htim16.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim16.Init.Period = 2005;
+  htim16.Init.Period = 335;
   htim16.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim16.Init.RepetitionCounter = 0;
   htim16.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
@@ -313,7 +313,7 @@ void MX_TIM16_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_ACTIVE;
-  sConfigOC.Pulse = 1433;
+  sConfigOC.Pulse = 239;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
